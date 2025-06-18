b2707ecf561df0f397f1b74b6610c01a00ca33f2
dhwoo <wooda01@naver.com>
2025-06-18 22:50:10 +0900

    feat: 회원 도메인 기초 틀 구현 #1

---
+++ src/main/java/spring/core/member/Member.java
@@ -0,0 +1,14 @@
+package spring.core.member;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Member {
+	private Long id;
+	private String name;
+	private Grade grade;
+}
