e976f18c39a166589d2f390fde3fa80aa791f123
dhwoo <wooda01@naver.com>
2025-07-16 22:22:58 +0900

    fix: AppConfig 구현에 따른 코드 수정 #7

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,17 +1,19 @@
 package spring.core.order;
 
 import spring.core.discount.DiscountPolicy;
-import spring.core.discount.FixDiscountPolicy;
-import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
-import spring.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-	private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+	private final MemberRepository memberRepository;
+	private final DiscountPolicy discountPolicy;
+
+	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
+	// 이렇게 변경함으로써 구체적인 클래스를 아예 모르고 인터페이스에만 의존하는 코드로 변경 -> DIP 철저히 준수
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
